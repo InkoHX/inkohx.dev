0132e044faf775fe2c899cfc4c13866234616867
InkoHX <me@inkohx.dev>
2024-06-21 00:58:59 +0900

    `@icons-pack/react-simple-icons`をmodularizeImportsを適用

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@icons-pack/react-simple-icons'],
+  },
+}
 
 export default nextConfig
