7b09de564c537e82e59b0aca3ed1f81bde8a61cf
InkoHX <me@inkohx.dev>
2024-06-20 16:37:07 +0900

    format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,27 @@
-import "./globals.css";
-
-import type { Metadata } from "next";
+import './globals.css'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "InkoHX's portfolio",
-  description: "InkoHXのポートフォリオ的なやつ",
+  description: 'InkoHXのポートフォリオ的なやつ',
   twitter: {
     card: 'summary_large_image',
     creator: 'InkoHX',
   },
   openGraph: {
     type: 'website',
-    siteName: 'InkoHX\' portfolio',
-  }
-};
+    siteName: "InkoHX' portfolio",
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="ja">
-      <body className="antialiased min-h-screen bg-slate-100">{children}</body>
+      <body className="min-h-screen bg-slate-100 antialiased">{children}</body>
     </html>
-  );
+  )
 }
