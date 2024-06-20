ebcb89184d35dfbb1378448966e3e39cea6f1f93
InkoHX <me@inkohx.dev>
2024-06-20 16:30:22 +0900

    setup Next.js project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # inkohx.dev
 
 > 🚧 このプロジェクトは**開発途中**であり、まだ inkohx.dev に反映されているわけではありません 🚧
-> 現在、最新のプレビューは[こちら](https://inkohx-dev.pages.dev)で確認することができます
+> 現在、最新のプレビューは[こちら](https://inkohx.vercel.app)で確認することができます
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "InkoHX's portfolio",
+  description: "InkoHXのポートフォリオ的なやつ",
+  twitter: {
+    card: 'summary_large_image',
+    creator: 'InkoHX',
+  },
+  openGraph: {
+    type: 'website',
+    siteName: 'InkoHX\' portfolio',
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className="antialiased min-h-screen bg-slate-100">{children}</body>
+    </html>
+  );
+}
