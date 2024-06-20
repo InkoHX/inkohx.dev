2653cf1278be92975f9b22bac0e253d5726c3a39
InkoHX <me@inkohx.dev>
2024-06-21 00:59:32 +0900

    ライセンス表記に関するページを追加

---
+++ src/app/licenses/[...slug]/page.tsx
@@ -0,0 +1,59 @@
+import licenses from '@/generated/license.json' with { type: 'json' }
+import { notFound } from 'next/navigation'
+
+type StaticParams = { slug: string[] }
+
+export const generateStaticParams = (): StaticParams[] => {
+  return Object.keys(licenses).map(packageName => ({
+    slug: packageName.split('/'),
+  }))
+}
+
+export default function PackageLicense({ params }: {
+  params: StaticParams
+}) {
+  const packageName = decodeURIComponent(params.slug.join('/'))
+  const data = licenses[packageName as keyof typeof licenses]
+
+  if (!data) notFound()
+
+  return (
+    <article className="py-8">
+      <main className="max-w-7xl mx-auto px-4">
+        <h1 className="text-4xl">
+          Thanks! <span className="font-bold">{packageName}</span>
+        </h1>
+        <dl className="mt-4 grid grid-cols-1 gap-2 sm:grid-cols-3">
+          <div>
+            <dt className="text-lg font-semibold">リポジトリ</dt>
+            <dd>
+              {data.repository ? (
+                <a
+                  className="text-primary-600 transition-opacity hover:underline hover:opacity-80"
+                  href={data.repository}
+                >
+                  {data.repository}
+                </a>
+              ) : (
+                '不明'
+              )}
+            </dd>
+          </div>
+          <div>
+            <dt className="text-lg font-semibold">ライセンスの種類</dt>
+            <dd>
+              {Array.isArray(data.licenses)
+                ? data.licenses.join(', ')
+                : data.licenses}
+            </dd>
+          </div>
+          <div>
+            <dt className="text-lg font-semibold">作者</dt>
+            <dd>{data.publisher ?? '不明'}</dd>
+          </div>
+        </dl>
+        <pre className="mt-4 overflow-x-auto rounded bg-slate-200 p-4">{data.licenseBody}</pre>
+      </main>
+    </article>
+  )
+}
---
+++ src/app/licenses/page.tsx
@@ -0,0 +1,128 @@
+import licenses from '@/generated/license.json' with { type: 'json' }
+import Link from 'next/link'
+
+export default function Licenses() {
+  return <article className="py-8">
+      <main className="max-w-7xl mx-auto px-4">
+        <section className="py-24">
+          <h1 className="text-7xl font-bold">ライセンス表記</h1>
+          <p className="mt-8 max-w-prose text-2xl text-slate-700">
+            このWebサイトで使用されているOSSのライセンス表記と、使われてるブランドアイコンの著作権を表記しているページです。
+          </p>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold">オープンソースソフトウェア</h2>
+          <ul className="mt-4 list-inside list-disc">
+            {Object.keys(licenses).map(packageName => (
+              <li key={packageName}>
+                <Link
+                  className="text-primary-600 hover:underline"
+                  href={`/licenses/${packageName}`}
+                >
+                  {packageName}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section className="mt-8 space-y-4">
+          <div>
+            <h2 className="text-4xl font-semibold">アイコンについて</h2>
+            <p className="mt-4">
+              このWebサイトで使われているアイコンの帰属先を記載しています。
+            </p>
+          </div>
+          <div>
+            <h3 className="text-2xl font-semibold">JetBrains logo</h3>
+            <pre className="mt-2 overflow-x-auto rounded bg-slate-200 p-4">
+              Copyright © 2000-{new Date().getUTCFullYear()}{' '}
+              <a
+                className="underline hover:text-primary-600"
+                href="https://www.jetbrains.com/"
+              >
+                JetBrains s.r.o
+              </a>
+              . JetBrains and the JetBrains logo are registered trademarks of
+              JetBrains s.r.o.
+            </pre>
+          </div>
+          <div>
+            <h3 className="text-2xl font-semibold">PHP logo</h3>
+            <pre className="mt-2 overflow-x-auto rounded bg-slate-200 p-4">
+              PHP logo by{' '}
+              <a
+                className="underline hover:text-primary-600"
+                href="https://viebrock.ca/"
+              >
+                Colin Viebrock
+              </a>{' '}
+              is licensed under{' '}
+              <a
+                href="https://creativecommons.org/licenses/by/4.0/"
+                className="underline hover:text-primary-600"
+              >
+                CC BY 4.0
+              </a>
+            </pre>
+          </div>
+          <div>
+            <h3 className="text-2xl font-semibold">Rust logo</h3>
+            <pre className="mt-2 overflow-x-auto rounded bg-slate-200 p-4">
+              Rust logo by{' '}
+              <a
+                className="underline hover:text-primary-600"
+                href="https://foundation.rust-lang.org/"
+              >
+                Rust Foundation
+              </a>{' '}
+              is licensed under{' '}
+              <a
+                href="https://creativecommons.org/licenses/by/4.0/"
+                className="underline hover:text-primary-600"
+              >
+                CC BY 4.0
+              </a>
+            </pre>
+          </div>
+          <div>
+            <h3 className="text-2xl font-semibold">Git logo</h3>
+            <pre className="mt-2 overflow-x-auto rounded bg-slate-200 p-4">
+              Git logo by{' '}
+              <a
+                className="underline hover:text-primary-600"
+                href="https://jasonlong.me/"
+              >
+                Jason Long
+              </a>{' '}
+              is licensed under{' '}
+              <a
+                href="https://creativecommons.org/licenses/by/3.0/"
+                className="underline hover:text-primary-600"
+              >
+                CC BY 3.0
+              </a>
+            </pre>
+          </div>
+          <div>
+            <h3 className="text-2xl font-semibold">Vue.js logo</h3>
+            <pre className="mt-2 overflow-x-auto rounded bg-slate-200 p-4">
+              Vue.js logo by{' '}
+              <a
+                href="http://evanyou.me/"
+                className="underline hover:text-primary-600"
+              >
+                Evan You
+              </a>{' '}
+              is licensed under{' '}
+              <a
+                href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+                className="underline hover:text-primary-600"
+              >
+                CC BY 4.0
+              </a>
+            </pre>
+          </div>
+        </section>
+      </main>
+    </article>
+}
