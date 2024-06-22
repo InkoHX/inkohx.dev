9d80d12b38ddbdc9e4965db1971d7ecfa3a005f9
InkoHX <me@inkohx.dev>
2024-06-22 23:45:34 +0900

    プロジェクトページを追加

---
+++ src/app/projects/Website.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import React from 'react'
+
+export interface WebsiteProps {
+  imageUrl: string
+  websiteName: string
+  link: string
+}
+
+export const Website: React.FC<WebsiteProps> = props => {
+  const imgRef = React.useRef<HTMLImageElement>(null)
+  const [imageWidth, setImageWidth] = React.useState(0)
+  const [imageHeight, setImageHeight] = React.useState(0)
+
+  React.useEffect(() => {
+    const img = imgRef.current
+    if (!img) return
+
+    setImageHeight(img.height)
+    setImageWidth(img.width)
+
+    const resizeObserver = new ResizeObserver((entries, _observer) => {
+      const { blockSize: height, inlineSize: width } =
+        entries[0].contentBoxSize[0]
+
+      setImageHeight(height)
+      setImageWidth(width)
+    })
+
+    resizeObserver.observe(img)
+    return () => resizeObserver.disconnect()
+  }, [])
+
+  return (
+    <a href={props.link} className="group relative block">
+      <img
+        src={props.imageUrl}
+        data-website-name={props.websiteName}
+        className="h-auto w-full group-hover:grayscale-0 sm:grayscale"
+        alt=""
+        ref={imgRef}
+      />
+      <div
+        className="absolute left-0 top-0 flex items-center justify-center bg-slate-200/70 px-8 text-4xl font-black transition lg:opacity-0 lg:hover:bg-slate-200/80 lg:hover:opacity-100"
+        style={{ width: `${imageWidth}px`, height: `${imageHeight}px` }}
+      >
+        {props.websiteName}
+      </div>
+    </a>
+  )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,243 @@
+import React from 'react'
+
+import { Website } from './Website'
+
+type CommonProjectInfo = {
+  name: string
+  description: string
+  repository: {
+    owner: string
+    name: string
+  }
+}
+
+type Website = {
+  name: string
+  link: string
+  imageUrl: string
+}
+
+type Tool = {
+  name: string
+  description: string
+  repository: {
+    owner: string
+    name: string
+  }
+}
+
+const packages = [
+  {
+    name: 'twitter-api-fetch',
+    description:
+      'fetch-like implementation designed for X (formerly Twitter) API',
+    repository: {
+      owner: 'nexterias',
+      name: 'twitter-api-fetch',
+    },
+  },
+  {
+    name: 'hono-cookie-signature',
+    description:
+      'A library for cookie signing and verification using HMAC-SHA256 for Hono.',
+    repository: {
+      owner: 'nexterias',
+      name: 'hono-cookie-signature',
+    },
+  },
+  {
+    name: 'discord.js-reaction-controller',
+    description:
+      'discord.js製のボット用に、リアクションを使ったページネーションをサポートする為のライブラリ',
+    repository: {
+      owner: 'InkoHX',
+      name: 'discord.js-reaction-controller',
+    },
+  },
+] as const satisfies ReadonlyArray<CommonProjectInfo>
+
+const tools = [
+  {
+    name: 'gh-archiver',
+    description: 'A tool for archiving GitHub repositories.',
+    repository: {
+      owner: 'InkoHX',
+      name: 'gh-archiver',
+    },
+  },
+  {
+    name: 'archiso',
+    description: '自分用のArch Linux installer',
+    repository: {
+      owner: 'InkoHX',
+      name: 'archiso',
+    },
+  },
+  {
+    name: 'actions-vercel',
+    description: 'Deploy to Vercel with GitHub Actions',
+    repository: {
+      owner: 'nexterias',
+      name: 'actions-vercel',
+    },
+  },
+  {
+    name: 'wakatime-tweet-activity',
+    description: 'WakaTimeで収集した情報をツイートするプログラム',
+    repository: {
+      owner: 'InkoHX',
+      name: 'wakatime-tweet-activity',
+    },
+  },
+] satisfies ReadonlyArray<Tool>
+
+const minecraftPlugins = [
+  {
+    name: 'player-tracker',
+    description: 'Assist in player tracking.',
+    repository: {
+      owner: 'nexterias',
+      name: 'player-tracker',
+    },
+  },
+  {
+    name: 'Chaining',
+    description: '鉱石や原木を一括破壊するプラグイン',
+    repository: {
+      owner: 'InkoHX',
+      name: 'Chaining',
+    },
+  },
+] satisfies ReadonlyArray<CommonProjectInfo>
+
+const bots = [
+  {
+    name: 'SpiderMonkey bot',
+    description: 'SpiderMonkey shell via Discord',
+    repository: {
+      owner: 'InkoHX',
+      name: 'sm-discord-bot',
+    },
+  },
+  {
+    name: 'HARZ',
+    description: 'Discord.js Japan User Group向けに開発していたDiscordボット',
+    repository: {
+      owner: 'InkoHX',
+      name: 'HARZ',
+    },
+  },
+  {
+    name: 'discord-link-viewer',
+    description: 'Discordのメッセージリンクから内容を表示する',
+    repository: {
+      owner: 'InkoHX',
+      name: 'discord-link-viewer',
+    },
+  },
+  {
+    name: 'nsfw-filter-bot',
+    description:
+      'NSFWチャンネル以外に投稿された性的な画像を削除するDiscordボット',
+    repository: {
+      owner: 'InkoHX',
+      name: 'nsfw-filter-bot',
+    },
+  },
+  {
+    name: 'volume-upper',
+    description:
+      '音声ファイルを音量5000dbでボイスチャンネルに再生するDiscordボット',
+    repository: {
+      owner: 'InkoHX',
+      name: 'volume-upper',
+    },
+  },
+] satisfies ReadonlyArray<CommonProjectInfo>
+
+const websites = [
+  {
+    name: 'Discord.js Japan User Group',
+    link: 'https://discordjs-japan.org',
+    imageUrl: '/img/websites/discordjs-japan-org.png',
+  },
+  {
+    name: 'The NEXTERIAS',
+    link: 'https://nexterias.dev',
+    imageUrl: '/img/websites/nexterias-dev.png',
+  },
+  {
+    name: 'RSS Reader',
+    link: 'https://news.inkohx.dev',
+    imageUrl: '/img/websites/news-inkohx-dev.png',
+  },
+  {
+    link: 'https://inkohx.dev',
+    name: 'Portfolio',
+    imageUrl: '/img/websites/inkohx-dev.png',
+  },
+] satisfies ReadonlyArray<Website>
+
+const ProjectList: React.FC<{
+  sectionName: string
+  projects: ReadonlyArray<CommonProjectInfo>
+}> = props => {
+  return (
+    <section className="mt-8">
+      <h2 className="text-4xl font-semibold">{props.sectionName}</h2>
+      <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-3">
+        {props.projects.map(project => (
+          <article
+            key={project.description}
+            className="h-full w-full rounded-md bg-slate-200 p-4 shadow-md"
+          >
+            <h3 className="text-2xl font-semibold">
+              <a
+                href={`https://github.com/${project.repository.owner}/${project.repository.name}`}
+                className="underline transition-colors hover:text-primary-600"
+              >
+                {project.name}
+              </a>
+            </h3>
+            <p className="mt-2">{project.description}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default function Projects() {
+  return (
+    <main className="mx-auto max-w-7xl px-4 py-8">
+      <div
+        className="bg-slate-100/90 bg-contain bg-right-top bg-no-repeat py-48 bg-blend-lighten lg:bg-blend-normal"
+        style={{ backgroundImage: "url('/img/undraw/projections.svg')" }}
+      >
+        <div className="max-w-prose">
+          <h1 className="text-7xl font-bold">プロジェクト</h1>
+          <p className="mt-8 text-4xl text-slate-700">
+            現在活発に開発してるものから、そうでないものまで紹介しちゃうぞ！
+          </p>
+        </div>
+      </div>
+      <ProjectList projects={packages} sectionName="パッケージ" />
+      <ProjectList projects={tools} sectionName="ツール" />
+      <ProjectList
+        projects={minecraftPlugins}
+        sectionName="Minecraftプラグイン"
+      />
+      <ProjectList projects={bots} sectionName="ボット" />
+      <section className="mt-8">
+        <h2 className="text-4xl font-semibold">ウェブサイト</h2>
+        <ul className="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+          {websites.map(({ imageUrl, link, name }) => (
+            <li className="w-full shadow-lg hover:shadow-xl" key={link}>
+              <Website imageUrl={imageUrl} link={link} websiteName={name} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  )
+}
