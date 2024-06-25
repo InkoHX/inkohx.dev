44d819fadb9d6daa99e6b1623122c17bfa1e486d
InkoHX <me@inkohx.dev>
2024-06-26 00:30:47 +0900

    configure `eslint-plugin-tailwindcss`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals'],
+  extends: ['next/core-web-vitals', 'plugin:tailwindcss/recommended'],
   plugins: ['simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'tailwindcss/classnames-order': 'off',
   },
   parserOptions: {
     sourceType: 'module',
