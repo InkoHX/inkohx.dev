c03d063157bc00b8446ecc338d86488794e35a21
InkoHX <me@inkohx.dev>
2024-06-26 15:42:46 +0900

    線のサイズを小さくする

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Timeline: React.FC<React.PropsWithChildren> = props => {
   return (
-    <div className="relative space-y-8 py-12 pl-16 before:absolute before:left-8 before:top-0 before:-z-10 before:h-full before:w-2 before:-translate-x-1/2 before:bg-slate-300 before:content-[''] lg:pl-0 lg:before:left-1/2">
+    <div className="relative space-y-8 py-12 pl-16 before:absolute before:left-8 before:top-0 before:-z-10 before:h-full before:w-1 before:-translate-x-1/2 before:bg-slate-300 before:content-[''] lg:pl-0 lg:before:left-1/2">
       {props.children}
     </div>
   )
