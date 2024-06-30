4d0e114973c6cd3be6543777a1931025642472eb
InkoHX <me@inkohx.dev>
2024-06-30 20:14:59 +0900

    列をもっと細かく制御

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -54,7 +54,7 @@ export default async function Articles({
           </p>
         </div>
       </div>
-      <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
+      <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {items.map((item, index) => (
           <Article key={index} {...item} />
         ))}
