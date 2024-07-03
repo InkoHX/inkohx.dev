3a3d9f1ebffb782c0b2067d75ec5797eb2634d1a
InkoHX <me@inkohx.dev>
2024-07-03 17:52:21 +0900

    モジュールパスを修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 
 import { AppFooter } from '@/components/AppFooter'
