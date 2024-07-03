8a822ca64a011b4765df8a2998c34dadab79091a
InkoHX <me@inkohx.dev>
2024-07-03 15:05:08 +0900

    コメント追加

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@icons-pack/react-simple-icons'],
     outputFileTracingIncludes: {
+      // OGP画像生成時、`posts`フォルダのMarkdownファイルを読み込もうとすると失敗する問題を修正するために設定
       '/articles/[articleId]/opengraph-image': ['./posts/*'],
     },
   },
