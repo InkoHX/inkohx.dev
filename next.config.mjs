713b4ae4ba14c323e8e1832e6e88aa8ebb1fd5d9
InkoHX <me@inkohx.dev>
2024-07-07 20:58:56 +0900

    CSPを設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,19 @@
+const developmentOnly = value =>
+  process.env.NODE_ENV === 'development' ? value : ''
+const contentSecurityPolicy = [
+  "default-src 'self'",
+  `script-src 'self' 'unsafe-inline' ${developmentOnly("'unsafe-eval'")} ${developmentOnly('https://va.vercel-scripts.com/v1/')}`,
+  "style-src 'self' 'unsafe-inline'",
+  "img-src 'self' https://res.cloudinary.com/zenn/image/upload/",
+  "object-src 'none'",
+  "base-uri 'self'",
+  "form-action 'self'",
+  "frame-ancestors 'none'",
+  'upgrade-insecure-requests',
+]
+  .map(it => it.trim())
+  .join('; ')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -6,6 +22,19 @@ const nextConfig = {
       // OGP画像生成時、`posts`フォルダのMarkdownファイルを読み込もうとすると失敗する問題を修正するために設定
       '/articles/[articleId]/opengraph-image': ['./posts/*'],
     },
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: contentSecurityPolicy,
+          },
+        ],
+      },
+    ]
   },
 }
 
