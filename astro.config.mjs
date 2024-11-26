5122d6762331c6bea0496eb2c87628580de20082
InkoHX <me@inkohx.dev>
2024-11-26 23:13:34 +0900

    明示的に`output`を`static`にする

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [sitemap(), tailwind(), react()],
   markdown: {
     shikiConfig: {
