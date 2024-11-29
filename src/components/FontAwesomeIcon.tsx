f2cb96c5ccad3a74ca8bb9e616b6b7cd02cb838f
InkoHX <me@inkohx.dev>
2024-11-29 12:01:20 +0900

    import stylesheet

--- src/components/FontAwesomeIcon.tsx
+++ src/components/FontAwesomeIcon.tsx
@@ -1,6 +1,7 @@
 import type { FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
 import { config as fontAwesomeConfig } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon as FontAwesomeIconOriginal } from '@fortawesome/react-fontawesome'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 fontAwesomeConfig.autoAddCss = false
 
