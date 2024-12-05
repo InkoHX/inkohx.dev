aaa753873dbc2207b230b5960174af7ae153fb35
InkoHX <me@inkohx.dev>
2024-12-05 22:56:10 +0900

    Astro v5に対応 (#239)

--- src/components/MobileHeaderNavigation.tsx
+++ src/components/MobileHeaderNavigation.tsx
@@ -2,20 +2,29 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 import constants from '../constants'
 import FontAwesomeIcon from './FontAwesomeIcon'
-import NavLink from './NavLink'
 
 const MobileHeaderNavigation: React.FC = () => {
   const [isOpen, setOpen] = React.useState(false)
+  const [pathname, setPathname] = React.useState<string | undefined>()
+
+  React.useEffect(() => {
+    setPathname(window.location.pathname)
+
+    const listener = () => setPathname(window.location.pathname)
+    window.addEventListener('popstate', listener)
+
+    return () => window.removeEventListener('popstate', listener)
+  }, [])
 
   return (
     <>
       <button type="button" className="sm:hidden" aria-label={isOpen ? 'メニューを閉じる' : 'メニューを開く'} onClick={() => setOpen(it => !it)}>
         {isOpen
           ? (
-              <FontAwesomeIcon icon={faXmark} className="size-8" />
+              <FontAwesomeIcon icon={faXmark} className="!size-8" />
             )
           : (
-              <FontAwesomeIcon icon={faBars} className="size-8" />
+              <FontAwesomeIcon icon={faBars} className="!size-8" />
             )}
       </button>
       <nav
@@ -23,14 +32,15 @@ const MobileHeaderNavigation: React.FC = () => {
         data-mobile-nav-open={isOpen}
       >
         {constants.navItems.map(({ text, href }) => (
-          <NavLink
+          <a
             key={text}
             href={href}
             onClick={() => setOpen(false)}
-            className="block rounded-lg p-4 text-center font-semibold transition-colors hover:bg-primary-100 data-[is-active=true]:bg-primary-200"
+            data-active={pathname === href}
+            className="block rounded-lg p-4 text-center font-semibold transition-colors hover:bg-primary-100 data-[active=true]:bg-primary-200"
           >
             {text}
-          </NavLink>
+          </a>
         ))}
       </nav>
     </>
