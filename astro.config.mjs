4dddeca35dd0d1700f7d0928ed0865fc6d6480c6
InkoHX <me@inkohx.dev>
2024-12-11 23:23:19 +0900

    環境変数をType-safeにする

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import process from 'node:process'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +12,15 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'dark-plus',
+    },
+  },
+  env: {
+    validateSecrets: true,
+    schema: {
+      OG_IMAGE_SIGNATURE_KEY: envField.string({
+        access: 'secret',
+        context: 'server',
+      }),
     },
   },
   server: {
