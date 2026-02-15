c4fa6dbf0724ca1472312fdae8092ec548f1a4d4
InkoHX <me@inkohx.dev>
2026-02-15 15:17:59 +0900

    fix: remove unused vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import process from 'node:process'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig, envField } from 'astro/config'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
